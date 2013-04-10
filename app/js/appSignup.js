14b03b2e5c4fd2450746950006bec6f27fd8bb69
Marwan Ghanem <marwan.t.ghanem@gmail.com>
2013-04-10 22:54:46 +0200

    sign up working but not as pretty as charles would like i guess

---
+++ app/js/appSignup.js
@@ -0,0 +1,19 @@
+ $(function(){
+	$("#signup").on("submit", function(){
+		var firstName = $("#first_name").val();
+		var lastName = $("#last_name").val();
+		var login = $("#login").val();
+		var password = $("#password").val();
+
+		Jwitter.signup(firstName,lastName,login,password,function(resp){
+			console.log(resp);
+
+			if(resp.error_code){
+				alert(resp.message);
+			}else
+				alert("done you can login now");
+
+			return false;
+		});
+});
+});
