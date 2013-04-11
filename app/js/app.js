06fb807f5d6e45739d6a47d11f72cf6e9e4a85d4
Charles-Emmanuel Dias <Charles-Emmanuel.dias@etu.upmc.fr>
2013-04-11 11:39:53 +0200

    cleaned master for branch work

--- app/js/app.js
+++ app/js/app.js
@@ -2,61 +2,66 @@
 $(function() {
 
 
-var env = new Environnement();
+	/*----------------
+	* Page  Variables |
+	* ---------------*/
+
+	var env = new Environnement();
 
 
- $("#login").on("submit", function(){
- 	var login = $("#login_value").val();
- 	var password = $("#password_value").val();
 
- 	Jwitter.login(login, password, function(resp){
+	/*------------------ * Events Bindings * -----------------------*/
 
- 		console.log(resp);
-
- 		if(!resp.error_code){
- 			var user = new User(resp.login,resp.id,resp.key,false);
- 			env = new Environnement(user);
-
- 			//setting cookie
- 			createCookie('JwitterUser',user,7);
- 		}
- 		else
- 			alert(resp.message);
-
- 	});
-
- 	return false; // deactivate page refresh 
- });
-
-
- $("#logout").on("submit", function(){
-
- 	eraseCookie('JwitterUser');
- 	env = new Environnement();
-
-
- 	return false; // deactivate page refresh 
- });
-
- $("#message_form").on("submit",function(){
 	
- 	var key = env.getKey();
- 	var message = $("#add_message");
- 	console.log(message); //try text
- 	return false;
- 	// Jwitter.post(key,message,function(resp){
+	/*--------Click login action-------------*/
 
- 	// 	if(resp.error_code){
- 	// 		alert(resp.message);
- 	// 	}
- 	// });
+	 $("#login").on("submit", function(){
+
+	 	var login = $("#login_value").val();
+	 	var password = $("#password_value").val();
+
+	 	Jwitter.login(login, password, function(resp){
+
+	 		if(!resp.error_code){
+	 			var user = new User(resp.login,resp.id,resp.key,false);
+	 			env = new Environnement(user);
+	 		}
+	 		else
+	 		{
+	 			alert(resp.message);
+	 		}
+	 	});
+
+	 	return false; // deactivate page refresh 
+	 });
 
 
- });
+
+	 /*--------Click logout action-------------*/
+
+	 $("#logout").on("submit", function(){
+
+	 	env = new Environnement();
+	 	return false; // deactivate page refresh 
+	 });
+
+
+
+	 /*-------------Click post action--------------*/
+
+	 $("#message_form").on("submit",function(){
+
+	 	alert('not implemented');
+	 	return false; // deactivate page refresh 
+	 });
+
+	 /*------------Click go(search) action------------*/
+	 
+	  $("#search_form").on("submit",function(){
+
+	 	alert('not implemented');
+	 	return false;// deactivate page refresh 
+	 });
+
+
 });
-
-
-function newPopup(url) {
-	popupWindow = window.open(
-		url,'popUpWindow','height=400,width=380,left=10,top=10,resizable=no,scrollbars=no,toolbar=no,menubar=no,location=no,directories=no,status=no,center=1')
-}
